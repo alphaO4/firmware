e0414863b74848bf9019dc7c15303689ff6e137c
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2024-02-17 04:15:06 +0100

    Updated hmac implementation

--- src/modules/hmac_sha256.cpp
+++ src/modules/hmac_sha256.cpp
@@ -1,102 +1,182 @@
-// hmac_sha256.cpp
-// Originally written in C by https://github.com/h5p9sl
-
+// File: hmac_sha256.cpp
+// Source: https://github.com/System-Glitch/SHA256
 #include "hmac_sha256.h"
-#include "sha256.h"
+#include <cstring>
+#include <sstream>
+#include <iomanip>
 
-#include <cstring> // For std::memcpy
-#include <vector>  // For std::vector
+constexpr std::array<uint32_t, 64> SHA256::K;
 
-constexpr size_t SHA256_BLOCK_SIZE = 64;
-
-// LOCAL FUNCTIONS
-
-// Concatenate X & Y, return hash.
-static void *H(const void *x,
-               const size_t xlen,
-               const void *y,
-               const size_t ylen,
-               void *out,
-               const size_t outlen);
-
-// Wrapper for sha256
-static void *sha256(const void *data,
-                    const size_t datalen,
-                    void *out,
-                    const size_t outlen);
-
-// Declared in hmac_sha256.h
-size_t hmac_sha256(const void *key,
-                   const size_t keylen,
-                   const void *data,
-                   const size_t datalen,
-                   void *out,
-                   const size_t outlen)
+SHA256::SHA256() : m_blocklen(0), m_bitlen(0)
 {
-    std::vector<uint8_t> k(SHA256_BLOCK_SIZE, 0);
-    std::vector<uint8_t> k_ipad(SHA256_BLOCK_SIZE, 0x36);
-    std::vector<uint8_t> k_opad(SHA256_BLOCK_SIZE, 0x5c);
-    uint8_t ihash[SHA256_HASH_SIZE];
-    uint8_t ohash[SHA256_HASH_SIZE];
-    size_t sz;
-
-    if (keylen > SHA256_BLOCK_SIZE)
-    {
-        // If the key is larger than the hash algorithm's
-        // block size, we must digest it first.
-        sha256(key, keylen, k.data(), k.size());
-    }
-    else
-    {
-        std::memcpy(k.data(), key, keylen);
-    }
-
-    for (size_t i = 0; i < SHA256_BLOCK_SIZE; ++i)
-    {
-        k_ipad[i] ^= k[i];
-        k_opad[i] ^= k[i];
-    }
-
-    // Perform HMAC algorithm: ( https://tools.ietf.org/html/rfc2104 )
-    //      `H(K XOR opad, H(K XOR ipad, data))`
-    H(k_ipad.data(), k_ipad.size(), data, datalen, ihash, sizeof(ihash));
-    H(k_opad.data(), k_opad.size(), ihash, sizeof(ihash), ohash, sizeof(ohash));
-
-    sz = (outlen > SHA256_HASH_SIZE) ? SHA256_HASH_SIZE : outlen;
-    std::memcpy(out, ohash, sz);
-    return sz;
+    m_state[0] = 0x6a09e667;
+    m_state[1] = 0xbb67ae85;
+    m_state[2] = 0x3c6ef372;
+    m_state[3] = 0xa54ff53a;
+    m_state[4] = 0x510e527f;
+    m_state[5] = 0x9b05688c;
+    m_state[6] = 0x1f83d9ab;
+    m_state[7] = 0x5be0cd19;
 }
 
-static void *H(const void *x,
-               const size_t xlen,
-               const void *y,
-               const size_t ylen,
-               void *out,
-               const size_t outlen)
+void SHA256::update(const uint8_t *data, size_t length)
 {
-    size_t buflen = xlen + ylen;
-    auto buf = new uint8_t[buflen];
+    for (size_t i = 0; i < length; i++)
+    {
+        m_data[m_blocklen++] = data[i];
+        if (m_blocklen == 64)
+        {
+            transform();
 
-    std::memcpy(buf, x, xlen);
-    std::memcpy(buf + xlen, y, ylen);
-    void *result = sha256(buf, buflen, out, outlen);
-
-    delete[] buf;
-    return result;
+            // End of the block
+            m_bitlen += 512;
+            m_blocklen = 0;
+        }
+    }
 }
 
-static void *sha256(const void *data,
-                    const size_t datalen,
-                    void *out,
-                    const size_t outlen)
+void SHA256::update(const std::string &data)
 {
-    Sha256Context ctx;
-    SHA256_HASH hash;
+    update(reinterpret_cast<const uint8_t *>(data.c_str()), data.size());
+}
 
-    Sha256Initialise(&ctx);
-    Sha256Update(&ctx, data, datalen);
-    Sha256Finalise(&ctx, &hash);
+std::array<uint8_t, 32> SHA256::digest()
+{
+    std::array<uint8_t, 32> hash;
 
-    size_t sz = (outlen > SHA256_HASH_SIZE) ? SHA256_HASH_SIZE : outlen;
-    return std::memcpy(out, hash.bytes, sz);
+    pad();
+    revert(hash);
+
+    return hash;
+}
+
+uint32_t SHA256::rotr(uint32_t x, uint32_t n)
+{
+    return (x >> n) | (x << (32 - n));
+}
+
+uint32_t SHA256::choose(uint32_t e, uint32_t f, uint32_t g)
+{
+    return (e & f) ^ (~e & g);
+}
+
+uint32_t SHA256::majority(uint32_t a, uint32_t b, uint32_t c)
+{
+    return (a & (b | c)) | (b & c);
+}
+
+uint32_t SHA256::sig0(uint32_t x)
+{
+    return SHA256::rotr(x, 7) ^ SHA256::rotr(x, 18) ^ (x >> 3);
+}
+
+uint32_t SHA256::sig1(uint32_t x)
+{
+    return SHA256::rotr(x, 17) ^ SHA256::rotr(x, 19) ^ (x >> 10);
+}
+
+void SHA256::transform()
+{
+    uint32_t maj, xorA, ch, xorE, sum, newA, newE, m[64];
+    uint32_t state[8];
+
+    for (uint8_t i = 0, j = 0; i < 16; i++, j += 4)
+    { // Split data in 32 bit blocks for the 16 first words
+        m[i] = (m_data[j] << 24) | (m_data[j + 1] << 16) | (m_data[j + 2] << 8) | (m_data[j + 3]);
+    }
+
+    for (uint8_t k = 16; k < 64; k++)
+    { // Remaining 48 blocks
+        m[k] = SHA256::sig1(m[k - 2]) + m[k - 7] + SHA256::sig0(m[k - 15]) + m[k - 16];
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        state[i] = m_state[i];
+    }
+
+    for (uint8_t i = 0; i < 64; i++)
+    {
+        maj = SHA256::majority(state[0], state[1], state[2]);
+        xorA = SHA256::rotr(state[0], 2) ^ SHA256::rotr(state[0], 13) ^ SHA256::rotr(state[0], 22);
+
+        ch = choose(state[4], state[5], state[6]);
+
+        xorE = SHA256::rotr(state[4], 6) ^ SHA256::rotr(state[4], 11) ^ SHA256::rotr(state[4], 25);
+
+        sum = m[i] + K[i] + state[7] + ch + xorE;
+        newA = xorA + maj + sum;
+        newE = state[3] + sum;
+
+        state[7] = state[6];
+        state[6] = state[5];
+        state[5] = state[4];
+        state[4] = newE;
+        state[3] = state[2];
+        state[2] = state[1];
+        state[1] = state[0];
+        state[0] = newA;
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        m_state[i] += state[i];
+    }
+}
+
+void SHA256::pad()
+{
+
+    uint64_t i = m_blocklen;
+    uint8_t end = m_blocklen < 56 ? 56 : 64;
+
+    m_data[i++] = 0x80; // Append a bit 1
+    while (i < end)
+    {
+        m_data[i++] = 0x00; // Pad with zeros
+    }
+
+    if (m_blocklen >= 56)
+    {
+        transform();
+        memset(m_data, 0, 56);
+    }
+
+    // Append to the padding the total message's length in bits and transform.
+    m_bitlen += m_blocklen * 8;
+    m_data[63] = m_bitlen;
+    m_data[62] = m_bitlen >> 8;
+    m_data[61] = m_bitlen >> 16;
+    m_data[60] = m_bitlen >> 24;
+    m_data[59] = m_bitlen >> 32;
+    m_data[58] = m_bitlen >> 40;
+    m_data[57] = m_bitlen >> 48;
+    m_data[56] = m_bitlen >> 56;
+    transform();
+}
+
+void SHA256::revert(std::array<uint8_t, 32> &hash)
+{
+    // SHA uses big endian byte ordering
+    // Revert all bytes
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            hash[i + (j * 4)] = (m_state[j] >> (24 - i * 8)) & 0x000000ff;
+        }
+    }
+}
+
+std::string SHA256::toString(const std::array<uint8_t, 32> &digest)
+{
+    std::stringstream s;
+    s << std::setfill('0') << std::hex;
+
+    for (uint8_t i = 0; i < 32; i++)
+    {
+        s << std::setw(2) << (unsigned int)digest[i];
+    }
+
+    return s.str();
 }
