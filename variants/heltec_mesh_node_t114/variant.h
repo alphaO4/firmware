4f24283606fd258b5e8814ac5634d1ca67654262
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2025-12-17 18:13:10 +0100

    Added Support for Default RTTTL PWM on Pin  0.28 on the Heltec t114

--- variants/heltec_mesh_node_t114/variant.h
+++ variants/heltec_mesh_node_t114/variant.h
@@ -206,6 +206,11 @@ No longer populated on PCB
 #define VBAT_AR_INTERNAL AR_INTERNAL_3_0
 #define ADC_MULTIPLIER (4.90F)
 
+// PWM Buzzer
+#define PIN_BUZZER (0 + 28)          // P0.28 for PWM buzzer output
+#define PIN_BUZZER_EN (0 + 28)       // Same pin, used by some code paths
+#define EXT_NOTIFY_OUT (0 + 28)      // Default external notification output
+
 #define HAS_RTC 0
 #ifdef __cplusplus
 }
